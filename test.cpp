f951d3f72bf3afdb2071d7bba5503269f24332a3
Carter S. <speers.carter@gmail.com>
2025-11-22 14:35:59 -0500

    added mock test

--- test.cpp
+++ test.cpp
@@ -6,15 +6,40 @@ TEST_CASE("Graph ADT", "[basic operations]") {
   REQUIRE(0 == 1); // not implemented yet so it fails.
 }
 
+TEST_CASE("Shortest Path of Flights", "[mock graph used]") {
+
+  Graph flightsNetwork;
+
+  flightsNetwork.addVertex("LAX");
+  flightsNetwork.addVertex("DEN");
+  flightsNetwork.addVertex("JFK");
+  flightsNetwork.addVertex("SUN");
+
+  flightsNetwork.addEdge("LAX", "DEN", 120, 0, 0);
+  flightsNetwork.addEdge("LAX", "SUN", 400, 0, 0);
+  flightsNetwork.addEdge("LAX", "JFK", 385, 0, 0);
+
+  flightsNetwork.addEdge("SUN", "DEN", 557, 0, 0);
+  flightsNetwork.addEdge("SUN", "LAX", 696, 0, 0);
+
+  flightsNetwork.addEdge("DEN", "SUN", 50, 0, 0);
+
+  flightsNetwork.addEdge("JFK", "SUN", 283, 0, 0);
+
+  auto shortest_route{std::vector<std::string>()};
+  auto dist = flightsNetwork.shortestPath("LAX", "SUN", shortest_route);
+
+  REQUIRE(dist == 170);
+  REQUIRE(shortest_route == std::vector<std::string>{"LAX", "DEN", "SUN"});
+}
+
 TEST_CASE("Shortest Path of Flights", "[test file used]") {
 
-  auto airportMap = loadAirportCodeMapCSV("airports.csv");
   auto flightsNetwork = loadFlightsCSV("flights-test-data.csv");
 
   auto shortest_route{std::vector<std::string>()};
   auto dist = flightsNetwork.shortestPath("LAX", "SUN", shortest_route);
 
   REQUIRE(dist == 170);
-  REQUIRE(shortest_route ==
-          std::vector<std::string>{"LAX", "DEN", "JFK", "SUN"});
+  REQUIRE(shortest_route == std::vector<std::string>{"LAX", "DEN", "SUN"});
 }
